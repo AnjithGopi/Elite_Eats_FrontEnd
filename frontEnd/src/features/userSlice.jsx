2dd1ec3af966745898ad92ccaa97d5949881b51b
AnjithGopi <ng.anjith444@gmail.com>
2025-05-15 22:42:34 +0530

    redux implementation started

---
+++ frontEnd/src/features/userSlice.jsx
@@ -0,0 +1,24 @@
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const userSlice=createSlice({
+
+    name:"user",
+    initialState:{
+
+        
+
+
+    },
+
+
+    reducers:{
+
+
+    }
+})
+
+
+export {}
